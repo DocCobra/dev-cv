bfff3ce48e9a67b669cc04ba270e9ddb2b9e5b73
DocCobra <daniele@torrini.org>
2022-08-21 14:51:43 +0200

    CV Entries constants file

---
+++ src/assets/entries.ts
@@ -0,0 +1,42 @@
+export const Entries = {
+    'experience': [
+        {
+            'title': 'Software Developer', 
+            'content': 'Stage scolastico come programmatore informatico in linguaggio C#. Creazione interfaccia client-server per conto di Sisal, mirata al trasferimento di strutture dati complesse tra un frontend Silverlight e un gestore di PDF ', 
+            'startDate': '06/06/2012',
+            'endDate': '30/06/2012'   
+        },
+        {
+            'title': 'Responsabile Informatico', 
+            'content': 'Tecnico informatico responsabile della gestione e manutenzione della strumentazione informatica (didattica e non) della scuola privata internazionale bilingue Kindergarten: computer amministrativi, Lim, tablet Android / iPad, registro elettronico, altro hardware (proiettori, stampanti ...).', 
+            'startDate': '15/11/2015',
+            'endDate': '01/03/2017'   
+        },
+        {
+            'title': 'Junior Virtual Reality Developer', 
+            'content': 'Stage formativo come sviluppatore C# e Blueprint (C++) per motori di sviluppo Unity3D e Unreal Engine 4 per la realizzazione individuale o in team di esperienze in Virtual e Augmented Reality, principalmente su device: Oculus Rift, HTC Vive, Microsoft HoloLens, Google Tango', 
+            'startDate': '01/05/2017',
+            'endDate': '01/08/2017'   
+        },
+        {
+            'title': 'Software Developer & IT Consultant', 
+            'content': 'Ricerca e sviluppo, anche in autonomia, di software proprietari per la gestione di challenge aziendali interne e di clienti nell\'ambito della sicurezza sul lavoro e dell\'organizzazione aziendale.\nIdeazione e realizzazione del nuovo sistema gestionale informatico aziendale su misura, per l\'interconnessione delle anagrafiche clienti, fornitori, gestione delle offerte e dei rapporti commerciali, rendicontazione dipendenti e spese, reportistica, sistema di fatturazione elettronica.', 
+            'startDate': '01/01/08',
+            'endDate': '-'
+        },
+    ],
+    'education': [
+        {
+            'title': 'Perito Industriale Capotecnico Specializ. Informatica', 
+            'content': 'Architettura IT\nSistemi Operativi e Network\nApplicazioni in C++, C#, Ambienti Database (MySQL).', 
+            'startDate': '01/09/2008',
+            'endDate': '30/06/2013'   
+        },
+        {
+            'title': 'Facoltà di Ingegneria Informatica', 
+            'content': 'Ingegneria del Software, Strutture e Architetture Dati, Analisi, IT Industriale, Telematica.', 
+            'startDate': '15/09/2013',
+            'endDate': '15/09/2014'   
+        }
+    ]
+};
