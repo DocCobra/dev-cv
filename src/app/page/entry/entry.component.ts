c0f263bf8ed92eb1bf0168173f27fdee78159803
DocCobra <daniele@torrini.org>
2022-08-22 12:35:50 +0200

    Removed breakpointobs (will handle with media qs)

--- src/app/page/entry/entry.component.ts
+++ src/app/page/entry/entry.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { BreakpointObserver } from '@angular/cdk/layout';
-
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
@@ -11,20 +9,10 @@ export class EntryComponent implements OnInit {
   @Input('title') entryTitle: string = '';  
   @Input('content') entryContent: string = ''; 
   @Input('startDate') entryStartDate: string = '';
-  @Input('endDate') entryEndDate: string = ''; 
+  @Input('endDate') entryEndDate: string = '';  
 
-  largeTitle: boolean = false; 
-  smallScreen: boolean = false; 
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor() { }
 
   ngOnInit(): void {
-    if (this.entryTitle.length >= 30) this.largeTitle = true; 
-    
-    this.breakpointObserver.observe([
-      '(max-width: 630px)'
-    ]).subscribe(state => {
-      this.smallScreen = state.matches ? true : false; 
-    });
   }
 }
