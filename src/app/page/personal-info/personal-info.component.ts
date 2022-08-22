73a81334adff694c090e144067a9d87ba47487e0
DocCobra <daniele@torrini.org>
2022-08-22 12:34:29 +0200

    Removed useless import

--- src/app/page/personal-info/personal-info.component.ts
+++ src/app/page/personal-info/personal-info.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BreakpointObserver } from '@angular/cdk/layout';
-
 @Component({
   selector: 'personal-info',
   templateUrl: './personal-info.component.html',
